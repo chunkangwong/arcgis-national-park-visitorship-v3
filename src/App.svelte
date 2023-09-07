d5e792f56aa78d0b45c4610260d3455e51577faa
ckwong <chunkang.wong94@gmail.com>
2023-09-08 02:45:23 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Map from "@arcgis/core/Map";
+  import MapView from "@arcgis/core/views/MapView";
+  import { onMount } from "svelte";
+
+  let viewDiv: HTMLDivElement;
+
+  onMount(() => {
+    const map = new Map({
+      basemap: "topo-vector",
+    });
+    const mapView = new MapView({
+      container: viewDiv,
+      map: map,
+      center: [-118.805, 34.027],
+      zoom: 13,
+    });
+  });
+</script>
+
+<main>
+  <div bind:this={viewDiv} />
+</main>
+
+<style>
+  main,
+  div {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+</style>
