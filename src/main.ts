0f9db1fd31ea1da1474c5888a1195d2d1d8394ea
ckwong <chunkang.wong94@gmail.com>
2023-09-08 02:48:32 +0800

    Added @esri/calcite-components dependency

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import "@esri/calcite-components/dist/calcite/calcite.css";
 import "./app.css";
 import App from "./App.svelte";
+
+import { defineCustomElements } from "@esri/calcite-components/dist/loader";
+
+defineCustomElements(window);
 
 const app = new App({
   target: document.getElementById("app")!,
